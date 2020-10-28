0ce64dc760f1df17db7c1944e2d17f0a4c6c62d5
AnnaGardt <60440521+AnnaGardt@users.noreply.github.com>
2020-10-28 13:31:30 +0700

    add files source + code day 1
    Добавлены файлы из архива для интенсива, написан код с первого дня интенсива

---
+++ js/main.js
@@ -0,0 +1,77 @@
+const cartButton = document.querySelector("#cart-button");
+const modal = document.querySelector(".modal");
+const close = document.querySelector(".close");
+
+cartButton.addEventListener("click", toggleModal);
+close.addEventListener("click", toggleModal);
+
+function toggleModal() {
+  modal.classList.toggle("is-open");
+}
+
+//day1
+
+const authButton = document.querySelector('.button-auth')
+const modalAuth = document.querySelector('.modal-auth')
+const closeButton = document.querySelector('.close-auth')
+const logInForm = document.querySelector('#logInForm')
+const loginInput = document.querySelector('#login')
+const buttonOut = document.querySelector('.button-out')
+const userName = document.querySelector('.user-name')
+
+let login = localStorage.getItem('gloDelivery')
+
+
+function toggleModalAuth() {
+  modalAuth.classList.toggle('is-open')
+}
+
+function authorized() {
+  function logout(){
+    login = null
+    localStorage.removeItem('gloDelivery')
+    buttonOut.style.display = ''
+    userName.textContent = ''
+    authButton.style.display = ''
+    buttonOut.removeEventListener('click', logout)
+    checkAuth()
+  }
+
+  console.log('Авторизован')
+  authButton.style.display = 'none'
+  buttonOut.style.display = 'block'
+  userName.style.display = 'inline-block'
+  userName.textContent = login
+  buttonOut.addEventListener('click', logout)
+}
+
+function notAuthorized() {
+  console.log('Не авторизован');
+
+  function logIn(ev) {
+    ev.preventDefault()
+    login = loginInput.value
+    localStorage.setItem('gloDelivery', login)
+    console.log(login)
+    toggleModalAuth()
+    authButton.removeEventListener('click', toggleModalAuth)
+    closeButton.removeEventListener('click', toggleModalAuth)
+    logInForm.removeEventListener('submit', logIn)
+    logInForm.reset()
+    checkAuth()
+  }
+
+  authButton.addEventListener('click', toggleModalAuth)
+  closeButton.addEventListener('click', toggleModalAuth)
+  logInForm.addEventListener('submit', logIn)
+}
+
+function checkAuth(){
+  if (login) {
+    authorized()
+  } else {
+    notAuthorized()
+  }
+}
+
+checkAuth()
